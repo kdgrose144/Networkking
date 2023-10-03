d3b14fea3f6124cd9cf708090ed8b16716fb0e27
kdgrose144 <146806264+kdgrose144@users.noreply.github.com>
2023-10-03 08:34:04 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,2 @@
+// JavaScript code can be added here to add interactivity to your website.
+// For example, you can use JavaScript to handle form submissions, create animations, or perform other tasks.

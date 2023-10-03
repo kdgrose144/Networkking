d682211ceb930959cb1f50ec477070e0cccd6bea
kdgrose144 <146806264+kdgrose144@users.noreply.github.com>
2023-10-03 08:42:26 +0200

    Delete script.js

--- script.js
+++
@@ -1,2 +0,0 @@
-// JavaScript code can be added here to add interactivity to your website.
-// For example, you can use JavaScript to handle form submissions, create animations, or perform other tasks.
